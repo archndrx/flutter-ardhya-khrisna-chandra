d7cd5fe4afafa770db5490f405764dbe113e1ebf
Chandra <khrisna302@gmail.com>
2023-03-30 22:28:18 +0700

    init Tugas 20

---
+++ 20_State_Management_Provider/praktikum/soal_app/lib/screen/imagepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:soal_app/model/images_model.dart';
+
+class ImagePage extends StatelessWidget {
+  final String imageUrl;
+
+  const ImagePage({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final imageUrl = args['imageUrl'];
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Image"),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Center(
+          child: Image.asset(imageUrl!),
+        ),
+      ),
+    );
+  }
+}
---
+++ 20_State_Management_Provider/readme.md
@@ -0,0 +1,6 @@
+1. State merupakan sebuah bentuk data yang hanya dapat dibaca saat terdapat perubahan widget
+2. Global state merupakan state yang dapat digunakan pada seluruh widget
+2. Global Widget dapat dimanfaatkan untuk memanfaatkan state yang sama dengan mudah
+
+
+link video demo : https://drive.google.com/file/d/1XAh0CVr4YXPs9QcW-rbCDLlLsho7YY4F/view?usp=sharing
